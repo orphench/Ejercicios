5a734a53ed3bb431789270423edf4a2a82234077
orphench <cris_alchemist8@hotmail.com>
2016-03-27 15:40:45 -0300

    Agregando otro ejercicio Signed-off-by: orphench <cris_alchemist8@hotmail.com>

---
+++ Ejercicio02/Ejercicio02/Ejercicio02/Ejercicio_02.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    class Ejercicio_02
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 02";
+
+            int numero;
+            string dato;
+            bool respuesta;
+
+            do
+            {
+                Console.WriteLine("Ingresa un numero mayor a cero (0): ");
+                dato = Console.ReadLine();
+                
+
+                while(int.TryParse(dato, out numero) == false)
+                {
+                    Console.WriteLine("Error, ingrese un numero valido: ");
+                    
+                    dato = Console.ReadLine();
+                }
+
+
+            } while (numero <= 0);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
