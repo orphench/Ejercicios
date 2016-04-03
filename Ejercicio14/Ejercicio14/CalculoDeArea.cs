c6d5ff2441b4ee069b828dd2691f0c3dec129ff6
orphench <cris_alchemist8@hotmail.com>
2016-04-03 20:26:19 -0300

    Ejercicio 14 sin main (hardcode) Signed-off-by: orphench <cris_alchemist8@hotmail.com>

---
+++ Ejercicio14/Ejercicio14/CalculoDeArea.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio14
+{
+    class CalculoDeArea
+    {
+        public static double area;
+
+        public static double CalcularCuadrado(double lado)
+        {
+            area = Math.Pow(lado, 2);
+
+            return area;
+        }
+
+        public static double CalcularTriangulo(double Base, double altura)
+        {
+            area = (Base * altura) / 2;
+
+            return area;
+        }
+
+        public static double CalcularCirculo(double radio)
+        {
+            area = Math.PI * Math.Pow(radio, 2);
+
+            return area;
+        }
+    }
+}
---
+++ Ejercicio14/Ejercicio14/Ejercicio_14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio14
+{
+    class Ejercicio_14
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 14";
+
+            
+
+            CalculoDeArea.CalcularCuadrado(4);
+            CalculoDeArea.CalcularTriangulo(5, 8);
+            CalculoDeArea.CalcularCirculo(3);
+
+        }
+    }
+}
