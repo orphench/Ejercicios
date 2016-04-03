562bb3c9857ebc92b4d9409832be8b9dbcd4d2a3
orphench <cris_alchemist8@hotmail.com>
2016-04-03 20:02:06 -0300

    ejercicio 12 Signed-off-by: orphench <cris_alchemist8@hotmail.com>

---
+++ Ejercicio12/Ejercicio12/Ejercicio_12.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio12
+{
+    class Ejercicio_12
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 12";
+
+            int numero;
+            int suma = 0;
+            bool resp = false;
+
+            
+            do
+            {
+                Console.WriteLine("Ingrese un numero a sumar: ");
+                numero = int.Parse(Console.ReadLine());
+
+                suma = numero + suma;
+                Console.Clear();
+
+                Console.WriteLine("Desea continuar? Ingrese S o N: ");
+                ValidarRespuesta.respuesta = Convert.ToChar(Console.ReadLine());
+                resp = ValidarRespuesta.ValidarS_N();
+                Console.Clear();
+            } while (resp == true);
+
+            Console.WriteLine("La suma de los numeros ingresados es: {0}", suma);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio12/Ejercicio12/ValidarRespuesta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio12
+{
+    class ValidarRespuesta
+    {
+        public static char respuesta;
+
+        public static bool ValidarS_N()
+        {
+            
+
+            while ( respuesta != 'S' && respuesta != 'N')
+            {
+                Console.Clear();
+                Console.WriteLine("Error, Ingrese S o N: ");
+                respuesta = Convert.ToChar(Console.ReadLine());
+
+            }
+
+            if (respuesta == 'S')
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
