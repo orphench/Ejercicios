2b29aaf2c74a0397c3cc9afeced6cb122bea9b9e
Lenovo <Lenovo@LENOVO-PC>
2016-05-11 02:03:46 -0300

    esqueleto ejercicio 42

---
+++ Ejercicio42/Ejercicio42/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    class Cliente
+    {
+        private string _apellido;
+        private int _cuentaBancaria;
+        private double _cuit;
+        private int _edad;
+        private string _nombre;
+
+        public string Apellido
+        {
+            get { return this._apellido; }
+            set { this._apellido = value; }
+        }
+
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return this._edad; }
+            set { this._edad = value; }
+        }
+
+        public double Cuit
+        {
+            get { return this._cuit; }
+            set { this._cuit = value; }
+        }
+
+        public int Cuenta
+        {
+            get { return this._cuentaBancaria; }
+        }
+
+        public Cliente(string apellido, string nombre, int cuenta)
+        {
+            this._apellido = apellido;
+            this._nombre = nombre;
+            this._cuentaBancaria = cuenta;
+        }
+    }
+}
---
+++ Ejercicio42/Ejercicio42/ClientePlus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    class ClientePlus:Cliente
+    {
+        private int _puntos;
+
+        public int Putos
+        {
+            get { return this._puntos; }
+            set { this._puntos = value; }
+        }
+
+        public ClientePlus(string apellido, string nombre, int cuenta, int puntos):base(apellido,nombre,cuenta)
+        {
+            this._puntos = puntos;
+        }
+    }
+}
